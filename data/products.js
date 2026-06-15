4e0055257da79a344c8ea78df3c4df684f33d83f
an3ssss <anesbenfardjallah921@gmail.com>
2026-06-15 08:55:51 +0100

    Hmida-pr-Image-Compression + blur-problem-fixed

--- data/products.js
+++ data/products.js
@@ -2,19 +2,19 @@ export  let products = [{
             title: 'Hmida - Product- 01',
             name: 'Hmida-product-01',
             price: 7900,
-            img01: 'Hmida-pics/Pdt01.PNG',
+            img01: 'Hmida-pics/Pdt01.avif',
             link: 'Hmida-product-01.html'
         }, {
             title: 'Hmida - Product- 02',
             name: 'Hmida-product-02',
             price: 6900,
-            img01: 'Hmida-pics/Pdt02.PNG',
+            img01: 'Hmida-pics/Pdt02.avif',
             link: 'Hmida-product-02.html'
         }, {
             title: 'Hmida - Product- 03',
             name: 'Hmida-product-03',
             price: 8900,
-            img01: 'Hmida-pics/Pdt03.PNG',
+            img01: 'Hmida-pics/Pdt03.avif',
             link: 'Hmida-product-03.html'
         }];
 
